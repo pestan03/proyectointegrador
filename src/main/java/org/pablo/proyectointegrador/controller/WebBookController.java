a335c3f41f0a3ed131306bdd3d97d9e963adf0ce
pab_san <147174341+pabsant@users.noreply.github.com>
2024-05-24 16:03:19 +0200

    search arreglado, solo falta mejorar interfaz

--- src/main/java/org/pablo/proyectointegrador/controller/WebBookController.java
+++ src/main/java/org/pablo/proyectointegrador/controller/WebBookController.java
@@ -5,11 +5,10 @@ import org.pablo.proyectointegrador.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/web/books")
@@ -38,11 +37,15 @@ public class WebBookController {
 
 
     @GetMapping("/search")
-    public String searchBook(Model model) {
-        model.addAttribute("books", bookService.getAllBooks());
+    public String searchBooks(@RequestParam(value = "query", defaultValue = "") String query, Model model) {
+        List<Book> books = bookService.getAllBooks().stream()
+                .filter(book -> book.getTitle().toLowerCase().contains(query.toLowerCase())
+                        || book.getAuthor().toLowerCase().contains(query.toLowerCase())
+                        || book.getIsbn().toLowerCase().contains(query.toLowerCase()))
+                .collect(Collectors.toList());
+        model.addAttribute("books", books);
         return "book-search";
     }
-
 
     @GetMapping("/edit/{id}")
     public String showEditBookForm(@PathVariable String id, Model model) {
